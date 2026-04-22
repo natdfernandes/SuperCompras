c05163c857e93659293e80e63d8a2a061e40e6d8
Natalia <nataliadiasfernande@gmail.com>
2026-04-22 13:56:02 -0300

    Projeto Super Compras

---
+++ app/src/main/java/com/example/supercompras/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.supercompras 
+
+import android.media.Image
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.supercompras.ui.theme.SuperComprasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperComprasTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+
+                    //ImagemTopo(modifier = Modifier.padding(innerPadding))
+                    //Titulo(modifier = Modifier.padding(innerPadding))
+                    Icone(
+                        modifier = Modifier.padding(innerPadding),
+                        icone = Icons.Default.Delete
+                        )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Titulo(modifier: Modifier = Modifier) {
+    Text(text = "Lista de Compras", modifier = modifier)
+}
+
+@Composable
+fun ImagemTopo(modifier: Modifier = Modifier) {
+    Image(painter = painterResource(R.drawable.image_topo),
+        contentDescription = null,
+        modifier = modifier.size(160.dp)
+    )
+    
+}
+
+@Composable
+fun Icone(icone: ImageVector, modifier: Modifier = Modifier) {
+    Icon(icone, contentDescription = "Editar", modifier = modifier)
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Natalia!",
+        modifier = modifier
+    )
+}
+
+@Preview
+@Composable
+private fun IconeEditPreview() {
+    SuperComprasTheme() {
+        Icone(icone = Icons.Default.Delete)
+    }
+    
+}
+
+@Preview
+@Composable
+private fun ImagemTopoPreview() {
+    SuperComprasTheme {
+      ImagemTopo()
+    }
+    
+}
+
+@Preview
+@Composable
+private fun TituloPreview() {
+    SuperComprasTheme() {
+        Titulo()
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperComprasTheme {
+        Greeting("Android")
+    }
+}
